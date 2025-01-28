0941bd708a465b5d3181f4df90ab32751dbfd941
Geovanny <geovanny.alcon2@gmail.com>
2025-01-27 22:52:48 -0600

    Modulo de agregacion de productos

--- Business Managment/Proyecto2GUI/BiblioAgregarLibro.Designer.cs
+++ Business Managment/Proyecto2GUI/BiblioAgregarLibro.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BiblioAgregarLibro));
-            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             label1 = new Label();
             panel1 = new Panel();
             button1 = new Button();
@@ -58,11 +58,11 @@
             label1.BackColor = Color.FromArgb(224, 224, 224);
             label1.Dock = DockStyle.Top;
             label1.FlatStyle = FlatStyle.Popup;
-            label1.Font = new Font("Arial Rounded MT Bold", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.FromArgb(244, 162, 97);
+            label1.Font = new Font("Segoe UI Semibold", 24F, FontStyle.Bold);
+            label1.ForeColor = Color.Black;
             label1.Location = new Point(0, 0);
             label1.Name = "label1";
-            label1.Size = new Size(1042, 190);
+            label1.Size = new Size(1042, 107);
             label1.TabIndex = 8;
             label1.Text = "Agregar Producto";
             label1.TextAlign = ContentAlignment.MiddleCenter;
@@ -85,9 +85,9 @@
             panel1.Controls.Add(label3);
             panel1.Controls.Add(label2);
             panel1.Dock = DockStyle.Top;
-            panel1.Location = new Point(0, 190);
+            panel1.Location = new Point(0, 107);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1042, 293);
+            panel1.Size = new Size(1042, 227);
             panel1.TabIndex = 9;
             // 
             // button1
@@ -103,7 +103,7 @@
             button1.ImageAlign = ContentAlignment.MiddleLeft;
             button1.Location = new Point(732, 122);
             button1.Name = "button1";
-            button1.Padding = new Padding(0, 0, 42, 0);
+            button1.Padding = new Padding(0, 0, 30, 0);
             button1.Size = new Size(151, 40);
             button1.TabIndex = 13;
             button1.Text = "Eliminar";
@@ -117,7 +117,7 @@
             txtprecio.BackColor = Color.SeaShell;
             txtprecio.BorderStyle = BorderStyle.None;
             txtprecio.Font = new Font("Microsoft Sans Serif", 10.8F);
-            txtprecio.Location = new Point(235, 202);
+            txtprecio.Location = new Point(242, 177);
             txtprecio.Name = "txtprecio";
             txtprecio.Size = new Size(437, 21);
             txtprecio.TabIndex = 12;
@@ -125,11 +125,11 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
-            label6.ForeColor = Color.FromArgb(244, 162, 97);
-            label6.Location = new Point(95, 198);
+            label6.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            label6.ForeColor = SystemColors.ActiveCaptionText;
+            label6.Location = new Point(102, 173);
             label6.Name = "label6";
-            label6.Size = new Size(93, 25);
+            label6.Size = new Size(77, 25);
             label6.TabIndex = 11;
             label6.Text = "PRECIO";
             // 
@@ -142,12 +142,12 @@
             btneditar.FlatAppearance.MouseOverBackColor = Color.FromArgb(230, 165, 36);
             btneditar.FlatStyle = FlatStyle.Flat;
             btneditar.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold);
-            btneditar.ForeColor = Color.White;
+            btneditar.ForeColor = Color.Black;
             btneditar.Image = (Image)resources.GetObject("btneditar.Image");
             btneditar.ImageAlign = ContentAlignment.MiddleLeft;
             btneditar.Location = new Point(732, 76);
             btneditar.Name = "btneditar";
-            btneditar.Padding = new Padding(0, 0, 42, 0);
+            btneditar.Padding = new Padding(0, 0, 47, 0);
             btneditar.Size = new Size(151, 40);
             btneditar.TabIndex = 10;
             btneditar.Text = "Editar";
@@ -165,14 +165,16 @@
             btnguardar.FlatAppearance.MouseOverBackColor = Color.FromArgb(230, 165, 36);
             btnguardar.FlatStyle = FlatStyle.Flat;
             btnguardar.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold);
-            btnguardar.ForeColor = Color.White;
+            btnguardar.ForeColor = Color.Black;
             btnguardar.Image = (Image)resources.GetObject("btnguardar.Image");
             btnguardar.ImageAlign = ContentAlignment.MiddleLeft;
             btnguardar.Location = new Point(732, 31);
             btnguardar.Name = "btnguardar";
+            btnguardar.Padding = new Padding(0, 0, 31, 0);
             btnguardar.Size = new Size(151, 40);
             btnguardar.TabIndex = 9;
             btnguardar.Text = "Guardar";
+            btnguardar.TextAlign = ContentAlignment.MiddleRight;
             btnguardar.UseVisualStyleBackColor = false;
             btnguardar.Click += btnRegistrarLibro_Click;
             // 
@@ -181,7 +183,7 @@
             txtcantidad.BackColor = Color.SeaShell;
             txtcantidad.BorderStyle = BorderStyle.None;
             txtcantidad.Font = new Font("Microsoft Sans Serif", 10.8F);
-            txtcantidad.Location = new Point(235, 160);
+            txtcantidad.Location = new Point(242, 135);
             txtcantidad.Name = "txtcantidad";
             txtcantidad.Size = new Size(437, 21);
             txtcantidad.TabIndex = 8;
@@ -191,7 +193,7 @@
             txtmarca.BackColor = Color.SeaShell;
             txtmarca.BorderStyle = BorderStyle.None;
             txtmarca.Font = new Font("Microsoft Sans Serif", 10.8F);
-            txtmarca.Location = new Point(235, 119);
+            txtmarca.Location = new Point(242, 94);
             txtmarca.Name = "txtmarca";
             txtmarca.Size = new Size(437, 21);
             txtmarca.TabIndex = 7;
@@ -201,7 +203,7 @@
             txtnombre.BackColor = Color.SeaShell;
             txtnombre.BorderStyle = BorderStyle.None;
             txtnombre.Font = new Font("Microsoft Sans Serif", 10.8F);
-            txtnombre.Location = new Point(235, 78);
+            txtnombre.Location = new Point(242, 53);
             txtnombre.Name = "txtnombre";
             txtnombre.Size = new Size(437, 21);
             txtnombre.TabIndex = 6;
@@ -211,7 +213,7 @@
             txtid.BackColor = Color.SeaShell;
             txtid.BorderStyle = BorderStyle.None;
             txtid.Font = new Font("Microsoft Sans Serif", 10.8F);
-            txtid.Location = new Point(235, 40);
+            txtid.Location = new Point(242, 15);
             txtid.Name = "txtid";
             txtid.Size = new Size(437, 21);
             txtid.TabIndex = 5;
@@ -219,44 +221,44 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
-            label5.ForeColor = Color.FromArgb(244, 162, 97);
-            label5.Location = new Point(95, 156);
+            label5.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            label5.ForeColor = SystemColors.ActiveCaptionText;
+            label5.Location = new Point(102, 131);
             label5.Name = "label5";
-            label5.Size = new Size(123, 25);
+            label5.Size = new Size(109, 25);
             label5.TabIndex = 3;
             label5.Text = "CANTIDAD";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
-            label4.ForeColor = Color.FromArgb(244, 162, 97);
-            label4.Location = new Point(95, 115);
+            label4.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            label4.ForeColor = SystemColors.ActiveCaptionText;
+            label4.Location = new Point(102, 90);
             label4.Name = "label4";
-            label4.Size = new Size(90, 25);
+            label4.Size = new Size(80, 25);
             label4.TabIndex = 2;
             label4.Text = "MARCA";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
-            label3.ForeColor = Color.FromArgb(244, 162, 97);
-            label3.Location = new Point(95, 74);
+            label3.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(102, 49);
             label3.Name = "label3";
-            label3.Size = new Size(104, 25);
+            label3.Size = new Size(92, 25);
             label3.TabIndex = 1;
             label3.Text = "NOMBRE";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
-            label2.ForeColor = Color.LightSalmon;
-            label2.Location = new Point(95, 36);
+            label2.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(102, 11);
             label2.Name = "label2";
-            label2.Size = new Size(33, 25);
+            label2.Size = new Size(32, 25);
             label2.TabIndex = 0;
             label2.Text = "ID";
             // 
@@ -264,38 +266,38 @@
             // 
             DGVArticulos.BackgroundColor = Color.White;
             DGVArticulos.BorderStyle = BorderStyle.None;
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = Color.LightCoral;
-            dataGridViewCellStyle3.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle3.ForeColor = Color.Azure;
-            dataGridViewCellStyle3.SelectionBackColor = Color.LightBlue;
-            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
-            DGVArticulos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = Color.LightCoral;
+            dataGridViewCellStyle1.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = Color.Azure;
+            dataGridViewCellStyle1.SelectionBackColor = Color.LightBlue;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            DGVArticulos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             DGVArticulos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = Color.SeaShell;
-            dataGridViewCellStyle4.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle4.ForeColor = Color.Black;
-            dataGridViewCellStyle4.SelectionBackColor = Color.PaleTurquoise;
-            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.False;
-            DGVArticulos.DefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = Color.SeaShell;
+            dataGridViewCellStyle2.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle2.ForeColor = Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = Color.PaleTurquoise;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            DGVArticulos.DefaultCellStyle = dataGridViewCellStyle2;
             DGVArticulos.GridColor = Color.FloralWhite;
             DGVArticulos.Location = new Point(123, 14);
             DGVArticulos.Margin = new Padding(3, 4, 3, 4);
             DGVArticulos.Name = "DGVArticulos";
             DGVArticulos.RowHeadersWidth = 51;
-            DGVArticulos.Size = new Size(801, 329);
+            DGVArticulos.Size = new Size(801, 271);
             DGVArticulos.TabIndex = 10;
             // 
             // panel2
             // 
             panel2.Controls.Add(DGVArticulos);
             panel2.Dock = DockStyle.Bottom;
-            panel2.Location = new Point(0, 481);
+            panel2.Location = new Point(0, 334);
             panel2.Name = "panel2";
-            panel2.Size = new Size(1042, 377);
+            panel2.Size = new Size(1042, 305);
             panel2.TabIndex = 10;
             // 
             // BiblioAgregarLibro
@@ -303,7 +305,7 @@
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(224, 224, 224);
-            ClientSize = new Size(1042, 858);
+            ClientSize = new Size(1042, 639);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(label1);
